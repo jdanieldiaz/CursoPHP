09286fda21250379e59c6e9681871bc7aa71fe41
jdanieldiaz <dandia28@gmail.com>
2021-03-22 23:14:38 -0300

    primer commit

---
+++ saludo.php
@@ -0,0 +1,10 @@
+<?php 
+
+    echo 'Hola Mundo' ; // Esto es un comentario de una línea
+    
+    /* Esto es
+    un comentario
+    en varias líneas */
+    
+    echo 'Adiós, hasta la próxima'; # Esto es otro comentario de una línea
+?>
